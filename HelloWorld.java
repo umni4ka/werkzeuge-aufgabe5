b5f76ce7584d793be12585f2641b752ff7bf0dcd
Rita Karam <rita.karam@student.uni-luebeck.de>
2016-11-08 11:45:06 +0100

    Hello World!

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,8 +2,4 @@ public class HelloWorld {
     public static void main (String[] args) {
 <<<<<<< HEAD
         System.out.println("Hello World");
-=======
-        System.out.println("Hallo Welt");
->>>>>>> origin/bugfix
-    }
-}
+
